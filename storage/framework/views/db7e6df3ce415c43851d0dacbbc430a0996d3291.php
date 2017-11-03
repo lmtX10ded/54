d8056eb851a565bdf054176710a334fdfb8acfdb
ryanmac51 <ryanmac51@Administrators-iMac-5.local>
2017-11-03 10:16:11 -0400

    nov 3

---
+++ storage/framework/views/db7e6df3ce415c43851d0dacbbc430a0996d3291.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Games</title>
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/sticky-footer.css" rel="stylesheet">
+</head>
